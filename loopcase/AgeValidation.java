e26197c4e2d220763d149033d3340166719c0480
Sahil Gupta <sgupta9557@gmail.com>
2024-09-17 10:41:38 -0700

    if-else condition

---
+++ loopcase/AgeValidation.java
@@ -0,0 +1,24 @@
+package loopcase;
+import java.util.Scanner;
+public class AgeValidation {
+	
+	public void validateage(int age) {
+		if(age<18)
+		{
+			System.out.println("Eligible");
+		}
+		else
+		{
+			System.out.println("Not Eligible");
+		}
+	}
+	public static void main(String[] args) {
+		AgeValidation ob = new AgeValidation();	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your age : ");
+		int age = sc.nextInt();
+		ob.validateage(age);
+		sc.close();
+		}
+
+}
