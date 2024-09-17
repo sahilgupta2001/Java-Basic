b08964ca8aafc99896287a3ea0665d4ae7116c33
Sahil Gupta <sgupta9557@gmail.com>
2024-09-17 10:49:36 -0700

    for loop condition

---
+++ loopcase/ten_to_one.java
@@ -0,0 +1,24 @@
+//Ten to one 
+package loopcase;
+
+public class ten_to_one {		//create a class
+	public static void main(String[] args) {
+		for(int i=10; i>=1; i--) {		//create a loop from 10 to 1
+			System.out.println(i);		//print 10 to 1 in descending order
+		}
+	}
+
+}
+//Results
+/*
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
