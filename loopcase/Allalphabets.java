33c18d501d2b5364160754e3acda3676fa517a30
Sahil Gupta <sgupta9557@gmail.com>
2024-09-16 22:55:42 -0700

    Print All alphabets using for

---
+++ loopcase/Allalphabets.java
@@ -0,0 +1,40 @@
+package loopcase;
+
+public class Allalphabets {		//create a class
+	public static void main(String[] args) {
+		for(char i='a'; i<='z'; i++)		//create a loop for alphabets
+		{
+			System.out.println(i);
+		}
+	}
+
+}
+//Results
+/*
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+*/
