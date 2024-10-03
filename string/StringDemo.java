50ca9c3147427871f4c422aa29a75142d95eb2d3
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2024-10-03 22:52:36 +0530

    String Demo
    Demo for String

---
+++ string/StringDemo.java
@@ -0,0 +1,13 @@
+package string;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = "java";
+		String s3 = new String("java");
+		String s4 = new String("java");
+		System.out.println(s1==s2);	//true
+		System.out.println(s2==s3);	//false
+		System.out.println(s3==s4);	//false
+	}
+}
