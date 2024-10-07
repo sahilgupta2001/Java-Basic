b3cfedaa2f3d239946062dfa17481095adaa99ba
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2024-10-07 22:10:45 +0530

    Update

---
+++ StaticDemo.java
@@ -0,0 +1,11 @@
+public class StaticDemo {  
+ static int a = 10;	
+ public StaticDemo()	
+{		
+  a++;		
+  System.out.println(a);
+	}	
+ public static void main(String[] args)	{		
+  StaticDemo ob = new StaticDemo();		
+  StaticDemo ob1 = new StaticDemo();		
+  StaticDemo ob2 = new StaticDemo();	}}
