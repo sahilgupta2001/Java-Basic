66a00844f36d91b465a5c599267ce4dd9740ea51
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2024-10-08 23:10:54 +0530

    Update array

---
+++ array/ArrayListDemo.java
@@ -0,0 +1,6 @@
+package array;
+import java.util.ArrayList;
+public class ArrayListDemo {
+	public static void main(String[] args) 	{
+		ArrayList list = new ArrayList();		
+ list.add("java");		list.add('a');		list.add(23);		list.add(34);				System.out.println(list);	}}
