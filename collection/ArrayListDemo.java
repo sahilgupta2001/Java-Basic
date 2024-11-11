0e5ce8e4b6e387ed42df5b641190c6689ce66da0
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2024-11-11 19:35:13 +0530

    ArrayListDemo

---
+++ collection/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) 
+	{
+		ArrayList list = new ArrayList();
+		list.add("java");
+		list.add('a');
+		list.add(23);
+		list.add(34);
+		System.out.println(list);
+		
+	}
+}
+// run : [java, a, 23, 34]
