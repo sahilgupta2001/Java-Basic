ad486b6ca4ea3c785920b773fd1458fc37b1f8cc
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2024-12-02 20:03:11 +0530

    Update StaticDemo.java

--- StaticDemo.java
+++ StaticDemo.java
@@ -1,18 +1,21 @@
-public class StaticDemo {  
- static int a = 10;	
- public StaticDemo()	
-{		
-  a++;		
-  System.out.println(a);
-	}	
- public static void main(String[] args)	{		
-  StaticDemo ob = new StaticDemo();		
-  StaticDemo ob1 = new StaticDemo();		
-  StaticDemo ob2 = new StaticDemo();	
+public class StaticDemo {
+static int a = 10;
+public StaticDemo() {
+	a++;
+	System.out.println(a);
 }
-}
+public static void main(String[] args)
+	{
+		
+  StaticDemo ob = new StaticDemo();
+		
+  StaticDemo ob1 = new StaticDemo();
+		
+  StaticDemo ob2 = new StaticDemo();
+}
+}
 /* console
 11
-11
-11
+12
+13
 */
